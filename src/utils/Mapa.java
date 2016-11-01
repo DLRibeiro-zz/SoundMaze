a74094cb7d896ec6e49d084b89a45f34a4b5a1db
rcalegario <rodrigocalegario@gmail.com>
2016-11-01 15:43:52 -0300

    inicializando a classe Mapa

---
+++ src/utils/Mapa.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class Mapa {
+
+	private Reta[] retas;
+	
+	public Mapa (int i){
+		retas = new Reta[i];
+	}
+
+	public Reta[] getRetas() {
+		return retas;
+	}
+
+	public void setRetas(Reta[] retas) {
+		this.retas = retas;
+	}
+	
+	public void criarMapa(){
+		//chamar a classe que Arthur esta implementado
+	}
+}
