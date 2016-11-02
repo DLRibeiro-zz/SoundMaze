7ef6f0e50bcb7eb73a3528d6eb4608f9e43c1cf4
WalbeRodri <wro@cin.ufpe.br>
2016-11-02 18:55:44 -0300

    inicio da integracao e confeccao do jogo

--- src/utils/Teste.java
+++ src/utils/Teste.java
@@ -3,6 +3,8 @@ package utils;
 import java.io.IOException;
 import java.util.Scanner;
 
+import sound.MultipleSound;
+
 public class Teste {
 
 	public static void main(String[] args) throws IOException {
@@ -10,7 +12,10 @@ public class Teste {
 		map.criarMapa();
 		System.out.println("Boneco: " + map.achaBoneco().toString());
 		System.out.println("Fonte: " + map.achaFonte().toString());
-
+		MultipleSound sound = new MultipleSound();
+		
+	
+		sound.execute(0.0f+map.achaBoneco().getX(), 0.0f+map.achaBoneco().getY(),0.0f+ map.achaBoneco().getZ());
 		map.printaTudo();
 		boolean exit = false;
 		Scanner in = new Scanner(System.in);
@@ -18,13 +23,14 @@ public class Teste {
 			String aux = in.nextLine();
 			System.out.println("\n");
 			switch (aux){ 
-				case "w": map.moveBoneco("w"); map.printaTudo();  break;
-				case "a": map.moveBoneco("a"); map.printaTudo();  break;
-				case "s": map.moveBoneco("s"); map.printaTudo();  break;
+				case "w": map.moveBoneco("w");sound.andar(0.0f+map.achaBoneco().getX(), 0.0f+map.achaBoneco().getY(),0.0f+ map.achaBoneco().getZ()); map.printaTudo();  break;
+				case "a": map.moveBoneco("a");sound.andar(0.0f+map.achaBoneco().getX(), 0.0f+map.achaBoneco().getY(),0.0f+ map.achaBoneco().getZ()); map.printaTudo();  break;
+				case "s": map.moveBoneco("s");sound.andar(0.0f+map.achaBoneco().getX(), 0.0f+map.achaBoneco().getY(),0.0f+ map.achaBoneco().getZ()); map.printaTudo();  break;
 				case "d": map.moveBoneco("d"); map.printaTudo();  break;
 				case "e": exit = !exit;  break;
 			}
 		}
+		in.close();
 	}
 
 }
